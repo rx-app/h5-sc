8e10e20eb530902ccfbaae58a513df6dd6bf58e4
rx-app <rxapp@yandex.com>
2020-05-25 18:21:37 +0800

    分享页恢复

--- src/views/Share.vue
+++ src/views/Share.vue
@@ -1,75 +1,205 @@
 <template>
   <div class="page-tips">
-    <router-link tag="div" :to="{name:'main'}" class="back"></router-link>
+    <!-- 这个页面不要自动格式化，Px会被转换掉 -->
+    <div @click="$router.push({name:'my'})" class="back"></div>
     <div class="top-title">测试大厅</div>
-    <div class="share-button"></div>
-    <img :src="urlImg" alt style="display:block;width:200px;height:200px" /> 
-    <div class="content" id="contentShare">
-      <div class="title">孤独症特质测试结果</div>
-      <div class="avatar"></div>
-      <div class="name">塔罗少年</div>
-      <div class="time">完成时间:2020.4.27</div>
-      <div class="res">本专题分享生日配对测试两人关系，姓名缘 分配对测试，婚姻配对测试。配对也就是双 方优劣互补的结果，若双方都是强势的人， 必须一</div>
-      <div class="qrcode" @touchstart="gotouchstart()" @touchmove="gotouchmove" @touchend="gotouchend"></div>
-      <div class="text">长按保存图片，分享给他人</div>
+    <div class="share-button" @click="saveImg"></div>
+    <div class="score-box">
+      <div class="left">
+        <div class="txt">您的总得分</div>
+        <div class="num">{{res.score}}</div>
+      </div>
     </div>
+    <div class="share-content">
+      <!-- <van-circle
+        class="circle"
+        v-model="currentRate"
+        :rate="res.score"
+        :speed="100"
+        :text="res.score.toString()"
+        layer-color="transparent"
+        :stroke-width="60"
+      /> -->
+
+      <div class="content">
+        <div class="user-info">
+          <div class="avatar">
+            <img v-if="!!avatar" :src="avatar" alt="">
+            <img v-else src="../assets/img/avatar.png" alt="">
+          </div>
+          <div class="center">
+            <div class="name">{{nickname}}</div>
+            <div class="time">完成时间:{{res.finish_at | dd}}</div>
+          </div>
+          <div class="right">
+            <span class="share-button"  @click="saveImg">分享</span>
+          </div>
+        </div>
+        <div class="test-result">
+          <div class="title">{{res.question_name}}</div>
+        
+        
+          <div class="res">{{res.result_content}}</div>
+          <div class="qrcode"></div>
+          <div class="text">长按保存图片，分享给他人</div>
+        </div>
+        
+      </div>
+    </div>
+    <van-popup position="bottom"  v-model="show">
+      <div class="popup">
+        <div class="top">
+          <!-- <div class="colse">关闭</div> -->
+          <!-- <a href=""></a> -->
+          <!-- <a href="" class="download">下载</a> -->
+          
+        </div>
+        <span @click="show=false" class="cha iconfont icon-cha"></span>
+        <div class="img-box">
+          <img class="share-img" src="" alt="">
+          
+        </div>
+      </div>
+    </van-popup>
   </div>
 </template>
 
 <script>
 import md5 from "js-md5";
 import html2canvas from "html2canvas";
+
 export default {
+  props: { id: 0, mid: 0 },
   data() {
     return {
-      timeOutEvent: "0",
-      urlImg: ""
+      currentRate: 50,
+      score: "80",
+      res: {},
+      show:false,
     };
   },
-  methods: {
-    gotouchstart() {
-      let that = this;
-      clearTimeout(this.timeOut); //清除定时器
-      this.timeOutEvent = 0;
-      this.timeOut = setTimeout(() => {
-        alert("11111111");
-        html2canvas(document.getElementById("contentShare"), {
-          allowTaint: false,
-          useCORS: true
-        }).then(canvas => { 
-          alert(222222)
-          // let dataUrl = canvas.toDataURL("image/jpeg");
-          this.urlImg = canvas.toDataURL(); 
-        }); 
-      }, 1000);
+  computed: {
+    avatar() {
+      return localStorage.getItem("avatar");
     },
-    //手释放，如果在500毫秒内就释放，则取消长按事件，此时可以执行onclick应该执行的事件
-    gotouchend() {
-      clearTimeout(this.timeOut);
-      if (this.timeOutEvent != 0) {
-        //这里写要执行的内容（尤如onclick事件）
-      }
+    nickname() {
+      return localStorage.getItem("nickname");
     },
-    //如果手指有移动，则取消所有事件，此时说明用户只是要移动而不是长按
-    gotouchmove() {
-      clearTimeout(this.timeOut); //清除定时器
-      this.timeOutEvent = 0;
+    level() {
+      return localStorage.getItem("level");
     }
   },
-  beforeDestroy() {
-    clearTimeout(this.timeOut);
+  created() {
+    this.getResult();
+  },
+  methods: {
+    saveImg() {
+      this.show = true
+      // 想要保存的图片节点
+      const dom = document.querySelector(".content");
+
+      // 创建一个新的canvas
+      const Canvas = document.createElement("canvas");
+      // const width = document.body.offsetWidth; // 可见屏幕的宽
+      // const height = document.body.offsetHeight; // 可见屏幕的高
+
+      const width = dom.offsetWidth; // 可见屏幕的宽
+      const height = dom.offsetHeight; // 可见屏幕的高
+      const scale = window.devicePixelRatio/2; // 设备的devicePixelRatio
+
+      // 将Canvas画布放大scale倍，然后放在小的屏幕里，解决模糊问题
+      Canvas.width = width * scale;
+      Canvas.height = height * scale;
+      Canvas.getContext("2d").scale(scale, scale);
+
+      html2canvas(dom, {
+        canvas: Canvas,
+        scale,
+        useCORS: true,
+        logging: true,
+        width: width + "px",
+        hegiht: height + "px"
+      }).then(canvas => {
+        const context = canvas.getContext("2d");
+        // 关闭抗锯齿形
+        context.mozImageSmoothingEnabled = false;
+        context.webkitImageSmoothingEnabled = false;
+        context.msImageSmoothingEnabled = false;
+        context.imageSmoothingEnabled = false;
+        // canvas转化为图片
+        const img = this.canvas2Image(canvas, canvas.width, canvas.height);
+        // document.body.appendChild(img);
+        // img.style.cssText =
+        //   "width:100%;height:100%;position:absolute;top:0;left:0;right:0;bottom:0;opacity:0;";
+      });
+    },
+    canvas2Image(canvas, width, height) {
+      const retCanvas = document.createElement("canvas");
+      const retCtx = retCanvas.getContext("2d");
+      retCanvas.width = width;
+      retCanvas.height = height;
+      retCtx.drawImage(canvas, 0, 0, width, height, 0, 0, width, height);
+      const img = document.querySelector(".share-img");
+      img.src = retCanvas.toDataURL("image/jpeg"); // 可以根据需要更改格式
+      // document.querySelector('.download').href=retCanvas.toDataURL("image/jpeg"); 
+      return img;
+    },
+
+    async getResult() {
+      let res = await this.$http.get(`test/question/result/${this.mid}`);
+      if (res.code == 200) {
+        console.log(res.data);
+      }
+      this.res = res.data;
+    }
   }
 };
 </script>
-
+<style>
+.van-circle__text {
+  color: #fff;
+  font-size: 60px;
+}
+</style>
 <style lang="scss" scoped>
+.img-box{
+  width: 100vw;
+  img{
+    width: 100vw;
+  }
+}
+.popup{
+  background:rgba(51,20,115,1);
+  .icon-cha{
+        color: #ADA7C1;
+        font-size: 40px;
+        position: absolute;
+        top:2px;
+        right: 32px;
+      }
+  .top{
+    background: #400e8d;
+    display: flex;
+    position: relative;
+    
+    div,a{
+      flex:1;
+      text-align: center;
+      font-size: 32px;
+      line-height: 64px;
+      color:#fff;
+    }
+  }
+}
 .page-tips {
-  height: 100vh;
+  // height: 100vh;
 
-  background: #261a58;
+  background: #400e8d;
+  height: 100%;
   background-size: cover;
   position: relative;
-  padding: 40px 0 0; // 50px -> 40px
+  // padding: 40px 0 0; // 50px -> 40px
+  // padding-top: 7px;
   .back {
     width: 72px;
     height: 68px;
@@ -81,56 +211,108 @@ export default {
   }
   .top-title {
     font-size: 48px;
+    line-height: 68px;
+    padding: 50px 0 45px 96px ;
     color: rgba(255, 255, 255, 1);
     // line-height:44px;
     text-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
-    margin: 20px 0px 75px 96px;
+        background: #261a58;
+    // margin: 0px 0px 45px 96px;  //页面的第一个子元素加了margin-top，导致页面顶部有白条
   }
-  .share-button {
-    width: 34px;
-    height: 34px;
-    position: absolute;
-    background: url("../assets/img/share.png") no-repeat;
-    background-size: contain;
-    top: 67px;
-    right: 28px;
+  // .share-button {
+  //   width: 34px;
+  //   height: 34px;
+  //   position: absolute;
+  //   background: url("../assets/img/share.png") no-repeat;
+  //   background-size: contain;
+  //   top: 67px;
+  //   right: 28px;
+  // }
+  .score-box{
+    background: #261a58;
+    .left{
+      padding: 20px 0 0 100px;
+      height: 190px;
+      width: 300px;
+      .txt{color:#fff;font-size:30px;line-height: 30px;margin-bottom: 10px;}
+      .num{color:#fff;font-size:85px;line-height: 80px;}
+
+    }
+  }
+  .circle {
+    width: 200px;
+    height: 200px;
+    margin-bottom: -50Px;
+    // margin-left: calc(50vw - 50Px);
   }
   .content {
     // margin-top: 270px;
-    height: calc(100vh - 50px - 20px - 48px - 270px + 100px);
+    // height: calc(100vh - 50px - 20px - 48px - 270px + 100px + 50Px);
     background: #400e8d;
-    overflow: scroll;
+    // overflow: scroll;
     border-radius: 53px 53px 0px 0px;
     padding: 180px 50px;
     font-size: 32px;
     color: #fff;
     line-height: 52px;
-    padding: 100px 72px 300px;
+    padding: 50px 22px 300px;
     text-align: center;
+    .user-info{
+      display: flex;
+      .avatar {
+          width: 136px;
+          height: 106px;
+          border-radius: 50%;
+          // background: url('../assets/img/img1.png') no-repeat;
+          background-size: cover;
+          img {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+          }
+          padding-left: 30px;;
+        }
+        .center{
+          margin-left: 25px;
+          .name {
+              font-size: 36px;
+              line-height: 36px;
+              color: #fff;
+              margin-bottom: 20px;
+              // font-weight: bold;
+              text-align: left;
+              padding-top: 20px;
+            }
+            .time {
+              font-size: 24px;
+              color: rgba(255, 255, 255, 0.39);
+              margin-bottom: 72px;
+            }
+        }
+        .right{
+          flex:1;
+          text-align: right;
+          padding: 25px 40px 0 0;
+          .share-button{
+            color:#fff;
+            font-size: 32px;
+            padding: 10px 25px;
+            line-height: 32px;
+            background: linear-gradient(90deg,rgba(72,197,255,1),rgba(144,77,255,1));
+            border-radius: 0 26px 26px 26px;
+          }
+
+        }
+        border-bottom: 1px solid rgb(78,25,157);
+    }
     .title {
       font-size: 40px;
       color: #fff;
       margin-bottom: 57px;
+      margin-top: 60px;
     }
-    .avatar {
-      width: 106px;
-      height: 106px;
-      border-radius: 50%;
-      background: url("../assets/img/img1.png") no-repeat;
-      background-size: cover;
-      margin-bottom: 25px;
-      margin-left: 250px;
-    }
-    .name {
-      font-size: 40px;
-      color: #fff;
-      margin-bottom: 34px;
-    }
-    .time {
-      font-size: 24px;
-      color: rgba(255, 255, 255, 0.39);
-      margin-bottom: 72px;
-    }
+    
+    
     .res {
       font-size: 32px;
       color: #fff;
@@ -138,16 +320,18 @@ export default {
       line-height: 52px;
     }
     .qrcode {
-      width: 234px;
-      height: 234px;
+      width: 200px;
+      height: 200px;
       background: url("../assets/img/qrcode.png") no-repeat;
+      background-size: 100% 100%;
       margin-bottom: 25px;
-      margin-left: 187px;
+      margin: auto;
     }
-    .title {
-      font-size: 40px;
+    .text {
+      font-size: 32px;
+      line-height: 32px;
       color: #fff;
-      margin-bottom: 57px;
+      margin-top: 27px;
     }
   }
 }
